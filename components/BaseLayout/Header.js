ec2a23431a66eb3f474d0f4e78f5a59684331f53
YaremenkoM <mariayaremenko13@gmail.com>
2022-04-02 00:59:13 +0200

    refactor base layout structure

---
+++ components/BaseLayout/Header.js
@@ -0,0 +1,13 @@
+import header from './../../styles/BaseLayout/Header.module.scss';
+
+const Header = () => {
+    return(
+        <header className={header.headerWrapper}>
+            <div className={header.header}>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
