aa3ca3fcf43703bd1728af776244d903494915f8
unknown <lastsymbol0@gmail.com>
2022-04-02 03:58:18 +0200

    feat(pages): add Team page

--- pages/team.js
+++ pages/team.js
@@ -1,14 +1,34 @@
-import Head from "next/head";
+import TeamMembersList from "../components/Team/TeamMembersList/TeamMembersList";
 
+import teamPage from './../styles/Team/TeamPage.module.scss';
+
+const TeamData = {
+    sections: [
+        {
+            title: "Team",
+            members: new Array(7).fill({
+                name: "Illia Abrosimov",
+                position: "Founder | CEO",
+                image: "/img/team/photo_ex.png",
+            })
+        },
+        {
+            title: "Advisors",
+            members: new Array(4).fill({
+                name: "Illia Abrosimov",
+                position: "Founder | CEO",
+                image: "/img/team/photo_ex.png",
+            })
+        }
+    ]
+}
 export default function Team(){
     return (
-        <>
-            <Head>
-                <title>Our team</title>
-            </Head>
-            <div>
-
-            </div>
-        </>
+        <div className={teamPage.teamPage}>
+            {
+                TeamData.sections.map(i =>
+                    <TeamMembersList title={i.title} members={i.members}/>)
+            }
+        </div>
     )
 }
